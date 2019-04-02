64e35838be0028a7294eaa50d96c80985392aec1
giuliom95 <giuliomartella1995@gmail.com>
2019-04-02 20:45:23 +0200

    Reading info file

--- imageconv.py
+++ imageconv.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from pathlib import Path
 from shutil import rmtree 
+from json import load
+
 
 def image_converter(path, out_dir):
     parent_dir = path.parent
@@ -27,9 +29,20 @@ out_projects.mkdir()
 
 in_projects_dir = Path.joinpath(in_dir, 'projects')
 
+projects = {}   # Stores info of all projects
+
 for in_proj_dir in in_projects_dir.iterdir():
 
-    out_proj_dir = Path.joinpath(out_projects, in_proj_dir.stem)
+    proj_id = in_proj_dir.stem
+    print(proj_id)
+
+    out_proj_dir = Path.joinpath(out_projects, proj_id)
     out_proj_dir.mkdir()
+
+    json_fp = open(str(Path.joinpath(in_proj_dir, 'info.json')), 'r')
+    info_json = load(json_fp)
+    projects[proj_id] = info_json
+
+    # Create thumbnails
     image_converter(Path.joinpath(in_proj_dir, 'preview.jpg'), out_proj_dir)
     
